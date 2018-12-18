afd0a0d155acebb03bdd21ad3817cf89ee217b91
Vulture <thomas.merel.44@gmail.com>
2018-12-18 02:24:28 +0100

    update cv

--- src/js/script.js
+++ src/js/script.js
@@ -3,10 +3,23 @@ var timeout = false;
 $(function () {
     new ClipboardJS(".clipboard");
     $('[data-toggle="tooltip"]').tooltip();
-});
 
-$.scrollify({
-    section: ".slide",
+    $.scrollify({
+        section: ".slide",
+    });
+
+    $("a[href*='#']:not([href='#'])").click(function() {
+        if (
+            location.hostname == this.hostname
+            && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
+        ) {
+            var anchor = $(this.hash);
+            anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
+            if ( anchor.length ) {
+                $("html, body").animate( { scrollTop: anchor.offset().top }, 1100);
+            }
+        }
+    });
 });
 
 function copyCp() {

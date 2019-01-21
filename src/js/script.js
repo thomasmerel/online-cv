d2de15b0891ecfc79d762465df309655d4418d5e
Vulture <thomas.merel.44@gmail.com>
2019-01-21 01:00:50 +0100

    update project + add text

--- src/js/script.js
+++ src/js/script.js
@@ -5,22 +5,28 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip();
 
     ifScrollify();
-    $(window).on('resize', function(){
+    $(window).on('resize', function () {
         ifScrollify();
     });
 
-    $("a[href*='#']:not([href='#'])").click(function() {
+    $("a[href*='#']:not([href='#'])").click(function () {
         if (
             location.hostname == this.hostname
-            && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
+            && this.pathname.replace(/^\//, "") == location.pathname.replace(/^\//, "")
         ) {
             var anchor = $(this.hash);
-            anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
-            if ( anchor.length ) {
-                $("html, body").animate( { scrollTop: anchor.offset().top }, 1100);
+            anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) + "]");
+            if (anchor.length) {
+                $("html, body").animate({scrollTop: anchor.offset().top}, 1100);
             }
         }
     });
+
+    calcAge();
+
+    setInterval(function () {
+        plusSlides(1);
+    }, 5000);
 });
 
 function ifScrollify() {
@@ -35,11 +41,11 @@ function ifScrollify() {
 }
 
 function copyCp() {
-    if(timeout === false) {
+    if (timeout === false) {
         $.notify({
             // options
             message: 'Copied to clipboard'
-        },{
+        }, {
             // settings
             type: 'dark',
             delay: 2000,
@@ -56,7 +62,45 @@ function copyCp() {
         timeout = true;
     }
 
-    setTimeout(function(){
+    setTimeout(function () {
         timeout = false;
     }, 3000);
+}
+
+function calcAge() { // birthday is a date
+    var birthday = new Date('1994-03-14');
+    var ageDifMs = Date.now() - birthday.getTime();
+    var ageDate = new Date(ageDifMs); // miliseconds from epoch
+    var age = Math.abs(ageDate.getUTCFullYear() - 1970);
+    $('#age').html(age)
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        //Hiding $(slides[i])
+        $(slides[i]).fadeOut('slow');
+    }
+    //Show $(slides[slideIndex-1])
+    setTimeout(function () {
+        $(slides[slideIndex - 1]).fadeIn('slow')
+    }, 600);
 }
